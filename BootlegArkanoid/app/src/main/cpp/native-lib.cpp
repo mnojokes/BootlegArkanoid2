436b6dac57fce9d7792cfda830bef51b09c72a33
Mantas Naujokas <mantas.naujokas@gmail.com>
2023-09-06 09:15:28 +0300

    Project setup completed.

--- BootlegArkanoid/app/src/main/cpp/native-lib.cpp
+++ BootlegArkanoid/app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_bootlegarkanoid_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello world!";
     return env->NewStringUTF(hello.c_str());
 }
